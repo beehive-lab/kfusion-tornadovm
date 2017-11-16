af5643aa2c448003a4c05bd35e56af2a473ccf50
Christos Kotselidis <christos.kotselidis@manchester.ac.uk>
2017-11-16 14:15:30 +0100

    Update license text

--- src/test/java/kfusion/pipeline/tornado/ReducePipeline.java
+++ src/test/java/kfusion/pipeline/tornado/ReducePipeline.java
@@ -1,17 +1,26 @@
 /*
- * Copyright 2017 James Clarkson.
+ *    This file is part of Slambench-Tornado: A Tornado version of the SLAMBENCH computer vision benchmark suite
+ *    https://github.com/beehive-lab/slambench-tornado
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *    Copyright (c) 2013-2017 APT Group, School of Computer Science,
+ *    The University of Manchester
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    This work is partially supported by EPSRC grants:
+ *    Anyscale EP/L000725/1 and PAMELA EP/K008730/1.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ *    Authors: James Clarkson
  */
 package kfusion.pipeline.tornado;
 
