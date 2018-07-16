27003406844f945f68e951989c9b81096587c0db
Juan Fumero <jjfumero@gmail.com>
2018-07-16 10:34:55 +0200

    runFPGA script updated with mm2metersKernel and bilateralFilter

--- runFPGA.sh
+++ runFPGA.sh
@@ -24,7 +24,22 @@ kfusion \
 -DrenderTrack.renderTrack.local.dims=64,16 \
 kfusion.tornado.Benchmark conf/bm-traj2.settings 
 
+elif [[ $1 == "mm2meters" ]] 
+then
+
+kfusion \
+-Dtornado.opencl.timer.kernel=False \
+-Dmm2metersKernel.mm2metersKernel.device=0:1 \
+-DbilateralFilter.bilateralFilter.device=0:1 \
+-Dtornado.opencl.codecache.loadbin=True \
+-Dtornado.precompiled.binary=fpgaKernels/mm2meters/lookupBufferAddress,mm2metersKernel.mm2metersKernel.device=0:1,fpgaKernels/mm2meters/lookupBufferAddress,bilateralFilter.bilateralFilter.device=0:1 \
+-DbilateralFilter.bilateralFilter.global.dims=320,240 \
+-DbilateralFilter.bilateralFilter.local.dims=64,16 \
+-Dmm2metersKernel.mm2metersKernel.global.dims=320,240 \
+-Dmm2metersKernel.mm2metersKernel.local.dims=64,16 \
+kfusion.tornado.Benchmark conf/bm-traj2.settings 
+
 else 
-        echo "Provide kernel to run on the FGPA: $ runFPGA.sh integrate|renderTrack " 
+        echo "Provide kernel to run on the FGPA: $ runFPGA.sh integrate|renderTrack|mm2meters " 
 fi
 
