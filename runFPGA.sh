d6652863b2eb337f9e433549d855694cffe70e87
Juan Fumero <jjfumero@gmail.com>
2018-07-17 14:49:28 +0200

    runFPGA raycast added

--- runFPGA.sh
+++ runFPGA.sh
@@ -39,6 +39,18 @@ kfusion \
 -Dpp.mm2meters.local.dims=64,16 \                                                                                                                                                                        
 kfusion.tornado.Benchmark conf/bm-traj2.settings  
 
+elif [[ $1 == "raycast" ]] 
+then
+
+kfusion \
+-Dtornado.opencl.timer.kernel=False \
+-Draycast.raycast.device=0:1 \
+-Dtornado.opencl.codecache.loadbin=True \
+-Dtornado.precompiled.binary=fpgaKernels/lookupBufferAddress,raycast.raycast.device=0:1 \
+-Draycast.raycast.global.dims=320,240 \
+-Draycast.raycast.local.dims=4,4 \
+kfusion.tornado.Benchmark conf/bm-traj2.settings 
+
 else 
         echo "Provide kernel to run on the FGPA: $ runFPGA.sh integrate|renderTrack|mm2meters " 
 fi
