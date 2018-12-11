fd84008dfd65c17c9f0949793d0c9fb1a93d5c42
Juan Fumero <juan.fumero@manchester.ac.uk>
2018-12-11 11:38:51 +0100

    Launcher with benchmarking mode OFF

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 echo "kfusion  kfusion.tornado.Benchmark conf/bm-traj2.settings "
 
-kfusion -XX:NativeMemoryTracking=summary -XX:+UnlockDiagnosticVMOptions -XX:+PrintNMTStatistics kfusion.tornado.Benchmark conf/bm-traj2.settings 
+kfusion -XX:NativeMemoryTracking=summary -XX:+UnlockDiagnosticVMOptions -XX:+PrintNMTStatistics -Dtornado.benchmarking=False kfusion.tornado.Benchmark conf/bm-traj2.settings 
+
