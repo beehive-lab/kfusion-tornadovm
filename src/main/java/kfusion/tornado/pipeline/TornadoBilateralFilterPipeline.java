84865c1842a1c84e5bdc9eb39b6eebf585f9becf
Juan Fumero <juan.fumero@manchester.ac.uk>
2019-02-07 14:42:58 +0100

    LICENCE header and file updated

--- src/main/java/kfusion/tornado/pipeline/TornadoBilateralFilterPipeline.java
+++ src/main/java/kfusion/tornado/pipeline/TornadoBilateralFilterPipeline.java
@@ -1,17 +1,26 @@
 /*
- * Copyright 2017 James Clarkson.
+ *  This file is part of Tornado-KFusion: A Java version of the KFusion computer vision
+ *  algorithm running on TornadoVM.
+ *  URL: https://github.com/beehive-lab/kfusion-tornadovm
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Copyright (c) 2013-2019 APT Group, School of Computer Science,
+ *  The University of Manchester
+ *
+ *  This work is partially supported by EPSRC grants Anyscale EP/L000725/1, 
+ *  PAMELA EP/K008730/1, and EU Horizon 2020 E2Data 780245.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 package kfusion.tornado.pipeline;
 
