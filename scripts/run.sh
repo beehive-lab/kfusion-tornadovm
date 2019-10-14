e22e849a40958b629d2e93d594d60645f6832c91
Juan Fumero <jjfumero@gmail.com>
2019-10-14 11:46:19 +0200

    Profiler disabled by default

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@
 
 echo "kfusion  kfusion.tornado.Benchmark conf/bm-traj2.settings "
 
-kfusion -Xms512m -Xmx512m -Dtornado.benchmarking=False -Dtornado.profiler=True -Dtornado.log.profiler=True kfusion.tornado.Benchmark conf/bm-traj2.settings 
+kfusion -Xms512m -Xmx512m -Dtornado.benchmarking=False -Dtornado.profiler=False -Dtornado.log.profiler=False kfusion.tornado.Benchmark conf/bm-traj2.settings 
 
