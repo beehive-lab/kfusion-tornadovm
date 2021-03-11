3d5fb1f68f96068041e1f0bcf867b335331ca28d
Michalis Papadimitriou <mikepapadim@hotmail.com>
2021-03-11 12:09:44 +0200

    Missing FMT conversion

--- src/main/java/kfusion/tornado/algorithms/IterativeClosestPoint.java
+++ src/main/java/kfusion/tornado/algorithms/IterativeClosestPoint.java
@@ -471,13 +471,13 @@ public class IterativeClosestPoint {
         solve(result.x, icpResults, 1);
 
         if (config.debug()) {
-            System.out.printf("\tx: %s\n", result.x.toString(FloatOps.fmt6e));
+            System.out.printf("\tx: %s\n", result.x.toString(FloatOps.FMT_6_E));
         }
 
         final Matrix4x4Float delta = new FloatSE3(result.x).toMatrix4();
 
         if (config.debug()) {
-            System.out.printf("*delta:\n%s\n", delta.toString(FloatOps.fmt4em));
+            System.out.printf("*delta:\n%s\n", delta.toString(FloatOps.FMT_4_EM));
             System.out.printf("*current pose:\n%s\n", currentPose.toString());
         }
 
