92178822f1fdd86c2dc042de7e6053668140108f
Juan Fumero <jjfumero@gmail.com>
2022-09-26 11:23:43 +0100

    Runner script updated with the latest TornadoVM version

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,11 @@
 
 echo "kfusion  kfusion.tornado.Benchmark conf/bm-traj2.settings "
 
-kfusion -Xms4g -Xmx4g -Dtornado.benchmarking=False -Dtornado.profiler=False -Dtornado.log.profiler=False kfusion.tornado.Benchmark conf/bm-traj2.settings 
+JARS=$(echo ${KFUSION_ROOT}/target/*.jar | tr ' ' ':')
+
+JFLAGS="-Xms4G -Xmx4G -Dtornado.kernels.coarsener=False -Dtornado.enable.fix.reads=False -Dlog4j.configurationFile=${KFUSION_ROOT}/conf/log4j2.xml -Dtornado.benchmarking=False -Dtornado.profiler=False -Dtornado.log.profiler=False"
+
+CLASSPATH=${CLASSPATH}:${JARS} tornado --jvm="${JFLAGS}" kfusion.tornado.Benchmark --params="conf/bm-traj2.settings" 
+
+#kfusion -Xms4g -Xmx4g -Dtornado.benchmarking=False -Dtornado.profiler=False -Dtornado.log.profiler=False kfusion.tornado.Benchmark conf/bm-traj2.settings 
 
